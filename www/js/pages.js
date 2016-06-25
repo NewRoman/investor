e300ccc89317ebf5414ea4b5464039830574e3e7
roma <romangorbunov22@gmail.com>
2016-06-25 23:29:05 +0300

    добавлена верстка 6-7страниц

--- www/js/pages.js
+++ www/js/pages.js
@@ -1,4 +1,42 @@
 $(document).ready(function(){
+
+	// show suitable radio block
+	function showRadioBlock($this) {
+		var value = parseInt($this.val()),
+			target = $this.data('target');
+
+		var $suitFieldsBlock = $('[data-radio-block-' + target + '="' + value + '"]').length ?
+			$('[data-radio-block-' + target + '="' + value + '"]') :
+			$('[data-radio-block-' + target + ']');
+
+		if ($('[data-radio-block-' + target + '="' + value + '"]').length > 0) {
+
+			$suitFieldsBlock.fadeIn();
+		} else {
+
+			$suitFieldsBlock.fadeOut();
+		}
+	}
+
+
+	// page_1 script show input if click Yes radio btn name=fail
+	$(document).on('change', 'input[name="fails"]', function(){
+		var value = parseInt($(this).val());
+
+		var $failInput = $('[data-radio-fail="' + value + '"]').length 
+			? $('[data-radio-fail="' + value + '"]')
+			: $('[data-radio-fail]');
+
+		if ($('[data-radio-fail="' + value + '"]').length > 0) {
+			$failInput.fadeIn();
+		} else {
+			$failInput.fadeOut();
+		}
+	});
+
+
+
+	// page_2 script
 	$(document).on('change', '#determine', function(){
 		if (parseInt($(this).find('option:selected').val()) == 5) {
 			$('.wrap-textarea').fadeIn();
@@ -6,7 +44,6 @@ $(document).ready(function(){
 			$('.wrap-textarea').fadeOut();
 		}
 	});
-
 	$(document).on('change', 'input[name="payback-equity"]', function(){
 		var value = parseInt($(this).val());
 
@@ -20,18 +57,30 @@ $(document).ready(function(){
 		}
 	});
 
-	// show input if click Yes radio btn name=fail
-	$(document).on('change', 'input[name="fails"]', function(){
-		var value = parseInt($(this).val());
 
-		var $failInput = $('[data-radio-fail="' + value + '"]').length 
-			? $('[data-radio-fail="' + value + '"]')
-			: $('[data-radio-fail]');
 
-		if ($('[data-radio-fail="' + value + '"]').length > 0) {
-			$failInput.fadeIn();
+	// page_6 script
+	$(document).on('change', 'input[name="any_transaction"]', function(){
+
+		showRadioBlock($(this));
+	});
+
+	// page_7 script
+	$(document).on('change', 'input[name="oper_history"]', function(){
+
+		showRadioBlock($(this));
+	});
+
+	$(document).on('change', 'input[name="prev_sold"]', function(){
+
+		showRadioBlock($(this));
+	});
+
+	$(document).on('focusout', 'input[name="how_sold"]', function(){
+		if (parseInt($(this).val()) > 100) {
+			$('[data-input-more-value]').fadeIn();
 		} else {
-			$failInput.fadeOut();
+			$('[data-input-more-value]').fadeOut();
 		}
 	});
 });
